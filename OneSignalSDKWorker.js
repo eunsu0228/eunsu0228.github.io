d2cf8d54187b6b05979ce42d8d935558d3868ba8
eunsu0228 <eunsu21107@gmail.com>
2026-02-24 15:08:39 +0900

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// 이 핸들러를 강제로 등록해서 브라우저의 불평(Error)을 잠재웁니다.
+self.addEventListener('message', (event) => {
+  // 메인 페이지와의 통신을 위한 빈 핸들러
+});
